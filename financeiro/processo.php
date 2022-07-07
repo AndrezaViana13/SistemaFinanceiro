ecfc7245466eba5d8a19caa424c22ceef0b6cc94
AndrezaViana13 <101840516+AndrezaViana13@users.noreply.github.com>
2022-07-07 20:11:22 -0300

    Update processo.php

--- financeiro/processo.php
+++ financeiro/processo.php
@@ -4,8 +4,18 @@
   $nome = $_POST['nome_cad'];
   $email = $_POST['email_cad'];
   $senha = $_POST['senha_cad'];
+  $nivel = 'Administrador';
 
+  $sql ="INSERT INTO usuarios(nome, email, senha, nivel) VALUES (:nome, :email, :senha, :nivel)";
 
-  $sql ="INSERT INTO usuarios(nome, email, senha, nivel) VALUES ($nome, $email, $senha, 'Administrador')";
+  $dados = $pdo->prepare($sql);
+  $dados->bindParam(':nome',$nome);
+  $dados->bindParam(':email',$email);
+  $dados->bindParam(':senha',$senha);
+  $dados->bindParam(':nivel',$nivel);
+  
+  $dados->execute();
+
+   
 
 ?>
